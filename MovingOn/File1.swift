4229628e336c9f8e77e3754d2f41a60cd5ca35f1
ctaicher <cltaicher@gmail.com>
2024-06-14 14:01:30 -0700

    Initial Commit

---
+++ MovingOn/File1.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  MovingOn
+//
+//  Created by Christopher Taicher on 6/14/24.
+//
+
+import Foundation
+
+##testing file stuff
+3rd stuff
---
+++ MovingOn/MovingOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingOnApp.swift
+//  MovingOn
+//
+//  Created by Christopher Taicher on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MovingOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
