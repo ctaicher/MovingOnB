5c32252ba6dcb7aa00b4549fae08e4aacbeb3fcc
ctaicher <cltaicher@gmail.com>
2024-06-14 14:01:30 -0700

    Initial Commit

---
+++ MovingOn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovingOn
+//
+//  Created by Christopher Taicher on 6/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello Christopher welcome to your new life, you'll get to see plenty of Lenni and have a great family!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
